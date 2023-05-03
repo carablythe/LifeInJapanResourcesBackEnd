bc5f195d8e65c143c69ff7a9c05999c2e2acb462
Cara Phillips <96596466+carablythe@users.noreply.github.com>
2023-05-04 00:35:38 +0900

    added .env file and installed some packages to get Postgres working

--- LifeInJapanResourcesBackEnd/settings.py
+++ LifeInJapanResourcesBackEnd/settings.py
@@ -15,11 +15,17 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import environ
+
+env = environ.Env()
+
+environ.Env.read_env()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ('django-insecure-%7-ij6vto^p!blnxphl)g46(%+7oe2mplq%qsf5+yfr&3&90_3')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,11 +80,19 @@ WSGI_APPLICATION = 'LifeInJapanResourcesBackEnd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+    'default': dj_database_url.parse(env('DATABASE_URL'))
+ 
 }
 
 
