a5cda247d85354aaf37496e77920bb6de6dc9f40
Cara Phillips <96596466+carablythe@users.noreply.github.com>
2023-05-04 02:32:32 +0900

    still working on django admin CSS issue

--- LifeInJapanResourcesBackEnd/settings.py
+++ LifeInJapanResourcesBackEnd/settings.py
@@ -22,13 +22,8 @@ env = environ.Env()
 
 environ.Env.read_env()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
